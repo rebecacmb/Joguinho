d7e17e32f5c3b7baf206170daa56dc2adad96efb
rebecacmb <129316755+rebecacmb@users.noreply.github.com>
2024-08-13 09:33:00 -0300

    dfgdfg

---
+++ script.js
@@ -0,0 +1,58 @@
+const caixaprincipal = document.querySelector('.caixa-principal')
+const caixaperguntas = document.querySelector('.caixa-perguntas')
+const caixaalternativas = document.querySelector('.caixa-alternativas')
+const caixaresultados = document.querySelector('.caixa-resultado')
+const caixaresultados = document.querySelector('.caixa-resultado')
+
+const perguntas = [
+
+    {
+        enunciado: "no ambito social, voce prefere:",
+        alternativas:
+            [
+
+                {
+                    texto: "conseguir a carreira dos sonhos e nunca mais poder faltar trabalho nem tirar férias, com um salário ótimo ",
+                    afirmação: "afirmação",
+                },
+
+                {
+                    texto: "passar na faculdade que você preferir,  nunca poder exercer o que você cursou,  trabalhar com algo que você odeia e tirar férias a cada 3 meses",
+                    afirmação: "afirmação",
+                }]
+    },
+    {
+        enunciado: "no ambito ambiental, voce prefere:",
+        alternativas:
+            [
+
+                {
+                    texto: "poder eliminar o desmatamento na amazônia e nunca mais poder realizar um sonho seu",
+                    afirmação: "afirmação",
+                },
+
+                {
+                    texto: "poder realizar um sonho na hora que quiser e morrer com o aumento do aquecimento global 1 semana depois  (você não pode desejar acabar com o aquecimento global)",
+                    afirmação: "afirmação",
+                }]
+    },
+    {
+        enunciado: "no ambito tecnologico, voce prefere:",
+        alternativas:
+            [
+
+                {
+                    texto: "a tecnologia avançar em um nível as pessoas não precisem fazer esforços físicos porém todos os seus dados e vida pessoal serão expostos ",
+                    afirmação: "afirmação",
+                },
+
+                {
+                    texto: "A tecnologia encontrar a cura para todas as doenças porém começaram a ser criados robôs para exilar a humanidade ",
+                    afirmação: "afirmação",
+                }]
+    },
+
+
+
+
+]
