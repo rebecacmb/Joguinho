0387b0bf6e69c572cc46fbceb532a3e85e56021a
rebecacmb <129316755+rebecacmb@users.noreply.github.com>
2024-08-20 09:27:12 -0300

    fghf

--- script.js
+++ script.js
@@ -13,12 +13,12 @@ const perguntas = [
 
                 {
                     texto: "conseguir a carreira dos sonhos e nunca mais poder faltar trabalho nem tirar férias, com um salário ótimo ",
-                    afirmação: "afirmação",
+                    afirmação: "parabens você, mas trabalha que nem uma vaca",
                 },
 
                 {
                     texto: "passar na faculdade que você preferir,  nunca poder exercer o que você cursou,  trabalhar com algo que você odeia e tirar férias a cada 3 meses",
-                    afirmação: "afirmação",
+                    afirmação: "parabens você trabalha com o que você odeia mas tem muito tempo livre",
                 }]
     },
     {
@@ -28,12 +28,12 @@ const perguntas = [
 
                 {
                     texto: "poder eliminar o desmatamento na amazônia e nunca mais poder realizar um sonho seu",
-                    afirmação: "afirmação",
+                    afirmação: "parabens você salvou o mundo mas não o seu",
                 },
 
                 {
                     texto: "poder realizar um sonho na hora que quiser e morrer com o aumento do aquecimento global 1 semana depois  (você não pode desejar acabar com o aquecimento global)",
-                    afirmação: "afirmação",
+                    afirmação: "parabens sua felicidade durou muito pouco",
                 }]
     },
     {
@@ -43,18 +43,19 @@ const perguntas = [
 
                 {
                     texto: "a tecnologia avançar em um nível as pessoas não precisem fazer esforços físicos porém todos os seus dados e vida pessoal serão expostos ",
-                    afirmação: "afirmação",
+                    afirmação: "parabens você é um preguiçoso com a vida exposta",
                 },
 
                 {
                     texto: "A tecnologia encontrar a cura para todas as doenças porém começaram a ser criados robôs para exilar a humanidade ",
-                    afirmação: "afirmação",
+                    afirmação: "parabens você salvou milhares de vidas mas provavelmente vai ser exilado por um robo",
                 }]
     },
 ]
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPerguntas (){
 
@@ -68,13 +69,16 @@ function mostraAlternativas() {
     for (const alternativa of perguntaAtual.alternativas)
     { const botaoAlternativa = document.createElement ("button")
         botaoAlternativa.textContent = alternativa.texto;
-        botaoAlternativa.addEventListener("click", function(){
-            atual++;
-            mostraPerguntas();
-        })
+        botaoAlternativa.addEventListener("click", ()=> respostaSelecionada(alternativa));
         caixaalternativas.appendChild(botaoAlternativa);
 
     }
+
+}
+function respostaSelecionada (alternativa){
+const afrimacoes = alternativa.afirmação;
+atual++;
+mostraAlternativas();
 
 }
 
